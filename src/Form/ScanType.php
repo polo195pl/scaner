4773ad26c2f3fc1634e78dab2ca85137602e9dc7
Patryk Tchorzewski <patryk@dark-side.pro>
2021-04-21 11:11:12 +0000

    finish scan form

--- src/Form/ScanType.php
+++ src/Form/ScanType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Scan;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,13 @@ class ScanType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('created_at')
-            ->add('code')
-            ->add('photo')
-            ->add('userId')
+            ->add('code', TextType::class)
+            ->add('photo', TextType::class, [
+                'attr' => ['class' => 'd-none hidden']
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-submit']
+            ])
         ;
     }
 
