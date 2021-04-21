ea4af948e4219504cee8d88f94c04c74779a711e
Patryk Tchorzewski <patryk@dark-side.pro>
2021-04-21 10:49:04 +0000

    init admin user form

---
+++ src/Form/AdminUserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('roles')
+            ->add('plainPassword', PasswordType::class, [
+                'maped' => false
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
